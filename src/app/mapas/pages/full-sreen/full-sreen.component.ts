b3dfa163b5b415ddc18e58fc0d6b1d6d412519cd
Dcterrance88 <>
2021-02-15 06:09:46 -0500

    instalando y configurando mapbox
    se realizo la instalacion de mapbox por medio de la copia del cdn en el
    index y la instalacion de una dependencia, se configuro el acceso al
    token de forma global en el componente principal y se configuro en el
    componente fullscreen la visualizacion del mapa añadiendo algunas
    caracteristicas.

--- src/app/mapas/pages/full-sreen/full-sreen.component.ts
+++ src/app/mapas/pages/full-sreen/full-sreen.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+// var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 @Component({
   selector: 'app-full-sreen',
   templateUrl: './full-sreen.component.html',
-  styles: [
-  ]
+  styles: [`
+  #mapa {
+    width: 100%;
+    height: 100%;
+  }
+  `]
 })
 export class FullSreenComponent implements OnInit {
+
 
   constructor() { }
 
   ngOnInit(): void {
+
+
+    var map = new mapboxgl.Map({
+    container: 'mapa',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-74.09476929690867, 4.746450340967757], //longitud, latitud
+    zoom: 16,
+    });
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiZGN0ZXJyYW5jZSIsImEiOiJja2w0aXZnbXMxdXo1MzFwbHVkMThsc25kIn0.VaUGoByofDRfUzpkT_nf5w',
 };
