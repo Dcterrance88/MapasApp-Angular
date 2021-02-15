eaf0d225fe88fe133e01fb4cea1a3743edaa2e57
Dcterrance88 <>
2021-02-15 10:55:24 -0500

    Creando el mapa y marcador en el comp marcadores
    se creo el mapa en dicho componente y se añadio de forma estatica un
    marcador para ver su funcionalidad.

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
-  styles: [
-  ]
+  styles: [`
+  .mapa-container{
+    width: 100%;
+    height: 100%;
+  }
+  `]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit{
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+
+  public mapa!: mapboxgl.Map;
+  public zoomLevel: number = 15;
+  public center: [number, number] = [-74.09476929690867, 4.746450340967757];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+
+      
   }
 
 }
